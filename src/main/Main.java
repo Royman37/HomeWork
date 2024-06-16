f98683a8e61becfd3307b102c58a582f2b797b12
Roman Korzhov <romakor1997@gmail.com>
2024-06-16 16:04:24 +0300

    Renaming package.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package Lesson_1;
+package main;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/PracticalTask.java
+++ src/main/PracticalTask.java
@@ -1,4 +1,4 @@
-package Lesson_1;
+package main;
 
 public class PracticalTask {
     /*
