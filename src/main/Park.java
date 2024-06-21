2d8d927c5e1523e58ad3fb65dfd93da7bc8cb158
Roman Korzhov <romakor1997@gmail.com>
2024-06-21 19:58:56 +0300

    correction pom.xml

---
+++ src/main/Park.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Park {
+    private String[] attractions;
+    private int workingHours;
+    private int cost;
+
+
+    class ParkManagement {
+        Park park = new Park();
+
+    }
+}
