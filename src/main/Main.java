d8c736e32c6e9ec0eb324ab2a6d779e05df44919
Roman Korzhov <romakor1997@gmail.com>
2024-06-29 16:01:04 +0300

    Lesson_9 corrected.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,8 @@ package main;
 public class Main {
     public static void main(String[] args) {
         MyArrayList array = new MyArrayList();
+        PhoneBook book = new PhoneBook();
         array.countWords();
+        book.lookingForANumber();
     }
 }
---
+++ src/main/PhoneBook.java
@@ -0,0 +1,40 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PhoneBook {
+    /*
+    Используем HasMap для создания хэш - карточки.
+    С помощью <Ключ, значение> находим нужный номер.
+     */
+    public void lookingForANumber() {
+        System.out.println("Task2: \n");
+        /*
+        Т.к. HaskMap перезаписывает ячейку, если указать идентичный ключ.
+         */
+        List<Map<String, String>> values = new ArrayList<>();
+        values.add(new HashMap<String, String>() {{
+            put("Roma", "+375 44 221 26 82");
+        }});
+        values.add(new HashMap<String, String>() {{
+            put("Petya", "+375 29 233 26 11");
+            put("Sam", "+375 29 126 72 11");
+        }});
+        values.add(new HashMap<String, String>() {{
+            put("Sam", "+375 44 261 13 36");
+        }});
+        values.add(new HashMap<String, String>() {{
+            put("Bob", "+375 29 111 11 21");
+        }});
+        System.out.println(values + "\n");
+
+        for (Map<String, String> map : values) {
+            if (map.containsKey("Sam")) {
+                System.out.println(map.get("Sam"));
+            }
+        }
+    }
+}
